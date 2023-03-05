e535f78c1c3acd52e23c7d05658e9a84ef063f8e
GOURAB ROY <91279162+gourabroyofficial@users.noreply.github.com>
2023-03-05 13:12:46 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,11 @@
+def factorial(num):
+    if num == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, num+1):
+            result *= i
+        return result
+
+num = 5
+print("The factorial of", num, "is", factorial(num))
